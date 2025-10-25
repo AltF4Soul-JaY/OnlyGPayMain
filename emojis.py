2018677ff0352bad936c3f57a3597cb4b92dafad
Naman Singh <107694918+AltF4Soul-JaY@users.noreply.github.com>
2025-10-25 11:43:46 +0530

    Create emojis.py

---
+++ emojis.py
@@ -0,0 +1,13 @@
+# emojis.py — central emoji map (use import EMOJI)
+EMOJI = {
+    "loading": "<a:loading_blue:1431120678416810045>",
+    "warn": "<a:animated_warn_sign:1431120898252996638>",
+    "disc": "<a:music_disc_animation:1422394071850291210>",
+    "gpay": "<:gpay:1421783759967228014>",
+    "pause": "<:pause:1431118802954879096>",
+    "resume": "<:resume:1431119137140379708>",
+    "sangeet": "<a:sangeet_gpay:1430820608697962526>",
+    "ticket_arrow": "<a:arrow_ticket:1423898387270467707>"
+}
+
+# Note: While using in your bot change these emojis to one in your server these won't work with your bot
